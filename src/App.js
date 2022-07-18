a0326fcc544538d2bcd49768b7732879fb0f01a2
Aizeee <isaac1chang@yahoo.com.sg>
2022-07-19 00:01:55 +0800

    testing on git merge on branch

--- src/App.js
+++ src/App.js
@@ -5,10 +5,11 @@
 } from "react-router-dom"
 
 
-
 import Home from "./pages/home/Home.js"
 import Hotel from "./pages/hotel/Hotel.js"
 import List from "./pages/list/List.js"
+
+/// testing git push, see whether there is a commit for git push on 
 
 function App() {
   return (
